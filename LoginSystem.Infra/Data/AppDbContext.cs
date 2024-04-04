128ca89d915063a164636eab6125b5e19d4477e6
Dener Niels <ksin_niels@outlook.com>
2024-04-04 06:22:18 +0000

    Criando DbContext

---
+++ LoginSystem.Infra/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LoginSystem.Core.Contexts.AccountContext.Entities;
+using LoginSystem.Infra.Contexts.AccountContext.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+    }
+}
