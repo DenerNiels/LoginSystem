db2411f6a1520901333aa0d95e5b6171c45e4121
Dener Niels <ksin_niels@outlook.com>
2024-04-08 06:41:08 +0000

    Response

--- LoginSystem.Core/Contexts/AccountContext/UseCases/Create/Response.cs
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Create/Response.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Create
 {
-    internal class Response
+    public class Response : SharedContext.UseCases.Response
     {
+        protected Response() 
+        {
+
+        }
+
+        public Response(string message, int status, IEnumerable<Notification>? notifications = null)
+        {
+            Message = message;
+            Status = status; 
+            Notifications = notifications;
+        }
+
+        public Response(string message,ResponseData data)
+        {
+            Message = message;
+            Status = 201;
+            data = data;
+        }
+
+        public  ResponseData? data { get; set; }
+
+        public record ResponseData(Guid Id, string Name, string Email);
     }
 }
---
+++ LoginSystem.Core/Contexts/SharedContext/UseCases/Response.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Core.Contexts.SharedContext.UseCases
+{
+    public abstract class Response
+    {
+        public string Message {  get; set; } = string.Empty;
+        public int Status { get; set; } = 400;
+        public bool IsSuccess => Status is >= 200 and <= 299;
+        public IEnumerable<Notification>? Notifications {  get; set; }
+    }
+}
