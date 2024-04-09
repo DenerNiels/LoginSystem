c2315ae6533c48dbe151cb8e3af9cac3915cb9af
Dener Niels <ksin_niels@outlook.com>
2024-04-09 07:16:02 +0000

    Valida a requisição

--- LoginSystem.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
@@ -16,10 +16,40 @@ namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Create
         {
             _repository = repository;
             _service = service;
-        } 
+        }
 
         public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
         {
+            #region 01. Valida a requisição
+            try
+            {
+                var res = Specification.Ensure(request);
+                if (!res.IsValid)
+                    return new Response("Requisição inválida", 400, res.Notifications);
+            }
+            catch (Exception e ) //ver aplicando oop na vida real
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            #endregion
+
+            #region 02. Gerar os objetos
+
+            #endregion
+
+            #region 03. Verificar o usuário
+
+            #endregion
+
+            #region 04. Persistir os dados 
+
+            #endregion
+
+            #region 05. Enviar E-mail de ativação
+
+            #endregion
 
         }
     }
