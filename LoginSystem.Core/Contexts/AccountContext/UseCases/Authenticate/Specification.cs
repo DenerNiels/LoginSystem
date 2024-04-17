5c1352d5844a840f76db785089f16232de97b684
Dener Niels <ksin_niels@outlook.com>
2024-04-17 07:13:36 +0000

    Authenticate Specification

--- LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Specification.cs
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Specification.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Authenticate
 {
-    public class Specification
+    public static class Specification
     {
+        public static Contract<Notification> Ensure(Request request)
+            => new Contract<Notification>()
+            .Requires()
+            .IsLowerThan(request.Password.Length, 40, "Password", "A senha deve conter menos de 40 caracteres")
+            .IsGreaterThan(request.Password.Length, 8, "Password", "A senha deve conter mais de 8 caracteres")
+            .IsEmail(request.Email, "E-mail", "E-mail inválido");
     }
 }
