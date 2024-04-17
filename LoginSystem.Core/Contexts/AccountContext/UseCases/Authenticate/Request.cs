e3b7a7f9e7de7a83ab29c8cbed626991c20dba6f
Dener Niels <ksin_niels@outlook.com>
2024-04-17 07:07:46 +0000

    Authenticate Request

--- LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Request.cs
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Request.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Authenticate
 {
-    public class Request
-    {
-    }
+    public record Request(
+        string Email,
+        string Password
+        ) : IRequest<Response>;
 }
