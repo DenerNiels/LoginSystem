a902fe0467fa02ac3abb3fef56c19b047947f71e
Dener Niels <ksin_niels@outlook.com>
2024-04-17 07:07:58 +0000

    Authenticate Response

--- LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Response.cs
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Authenticate/Response.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Authenticate
 {
-    public class Response
+    public class Response : SharedContext.UseCases.Response
     {
+        protected Response()
+        {
+
+        }
+
+        public Response(string message, int status, IEnumerable<Notification>? notifications = null)
+        {
+            Message = message;
+            Status = status;
+            Notifications = notifications;
+        }
+
+        public Response(string message, ResponseData data)
+        {
+            Message = message;
+            Status = 201;
+            data = data;
+        }
+
+        public ResponseData? Data { get; set; }
+
+        public class ResponseData
+        {
+            public string Token { get; set; } = string.Empty;
+            public string Id { get; set; } = string.Empty;
+            public string Name { get; set; } = string.Empty;
+            public string Email { get; set; } = string.Empty;
+            public string[] Roles { get; set; } = Array.Empty<string>();
+        }
     }
 }
