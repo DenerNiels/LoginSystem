241a7e515e5e64b1d9e5ac40a64efd97746764ac
Dener Niels <ksin_niels@outlook.com>
2024-04-20 15:17:16 +0000

    JwtService

---
+++ LoginSystem.Api/Extensions/LsExtension.cs
@@ -0,0 +1,41 @@
+﻿using LoginSystem.Core;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+using static LoginSystem.Core.Contexts.AccountContext.UseCases.Authenticate.Response;
+
+namespace LoginSystem.Api.Extensions
+{
+    public static class LsExtension
+    {
+        public static string Generate(ResponseData data)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.Secrets.JwtPrivateKey);
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = GenerateClaims(data),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = credentials,
+            };
+            var token = handler.CreateToken(tokenDescriptor);
+            return handler.WriteToken(token);
+        }
+
+        public static ClaimsIdentity GenerateClaims(ResponseData user)
+        {
+            var ci = new ClaimsIdentity();
+            ci.AddClaim(new Claim("Id", user.Id));
+            ci.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
+            ci.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+            foreach (var role in user.Roles)
+                ci.AddClaim(new Claim(ClaimTypes.Role, role));
+
+            return ci;
+        }
+    }
+}
