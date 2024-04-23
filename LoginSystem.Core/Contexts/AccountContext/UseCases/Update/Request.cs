2df9fb7d88535b4f3816f3cbd1a0d3dc6ebaf713
Dener Niels <ksin_niels@outlook.com>
2024-04-23 07:32:13 +0000

    update

---
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Update/Request.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Update
+{
+    public record Request(
+        string Name,
+        string Email,
+        string Password
+        ) : IRequest<Response>;
+}
---
+++ LoginSystem.Core/Contexts/AccountContext/UseCases/Update/Response.cs
@@ -0,0 +1,28 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem.Core.Contexts.AccountContext.UseCases.Update
+{
+    public class Response : SharedContext.UseCases.Response
+    {
+        protected Response()
+        {
+
+        }
+
+        public Response(string message, int status, IEnumerable<Notification>? notifications = null)
+        {
+            Message = message;
+            Status = status;
+            Notifications = notifications;
+        }
+        public ResponseData? Data { get; set; }
+
+        public record ResponseData(Guid Id, string Name, string Email);
+
+    }
+}
